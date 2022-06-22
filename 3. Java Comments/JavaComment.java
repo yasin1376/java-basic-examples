206b12e3f7d3601b9d35e31f8d1c7bba48d3461b
Yasin <yasinm289@gmail.com>
2022-06-22 11:47:52 +0430

    added java comments

---
+++ 3. Java Comments/JavaComment.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaComment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // This is a single-line comment
+        
+        System.out.println("Hello World");
+        
+        /* This is a multi-line comment & 
+            
+            It is amazing! */
+        
+        System.out.println("Hello World");
+
+    }
+}
