2aeaab1131220b8f5d3fafff488c87fdb6288553
Yasin <yasinm289@gmail.com>
2022-06-22 11:18:41 +0430

    added java output

---
+++ 2. Java Output/JavaOutput.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaOutput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        
+        System.out.println(3 + 3);          // output the numbers
+        
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+        
+    }
+}
