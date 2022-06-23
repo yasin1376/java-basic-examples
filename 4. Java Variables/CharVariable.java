0af4777ae5065840fa123ab4035e193368e9940f
Yasin <yasinm289@gmail.com>
2022-06-23 13:27:16 +0430

    char variable added

---
+++ 4. Java Variables/CharVariable.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class CharVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+        
+        char myLetter = 'D';
+        
+        System.out.print(myLetter);
+       
+    }
+}
