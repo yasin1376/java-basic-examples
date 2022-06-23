6b7bd143b9b1650edca40f6b8a672aba4ef31b0d
Yasin <yasinm289@gmail.com>
2022-06-23 10:22:39 +0430

    added float variable

---
+++ 4. Java Variables/FloatVariable.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class FloatVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        
+        float myFloatNum = 5.99f;
+        
+        System.out.print(myFloatNum);
+    }
+}
