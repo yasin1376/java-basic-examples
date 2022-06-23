54022d504b93227869e04e91ab039252f72b616b
Yasin <yasinm289@gmail.com>
2022-06-23 13:37:53 +0430

    added boolean variable

---
+++ 4. Java Variables/BooleanVariable.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class BooleanVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // boolean - stores values with two states: true or false
+        
+        boolean myBool = true;
+        
+        System.out.print(myBool);
+    }
+}
