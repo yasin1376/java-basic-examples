f335a1a1de18ae9dda9f8a4f23c6580674608fcc
Yasin <yasinm289@gmail.com>
2022-06-23 10:13:05 +0430

    int variable added

---
+++ 4. Java Variables/IntVariable.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class IntVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        
+        int myNum = 15;
+        
+        System.out.println(myNum);
+        
+        // You can also declare a variable without assigning the value, and assign the value later
+        
+        int num;
+        
+        num = 5;
+        
+        System.out.println(num);
+        
+    }
+}
