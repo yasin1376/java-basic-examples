5c1c4a4ef4792d90cc59bd23b5c98d461038b843
Yasin <yasinm289@gmail.com>
2022-06-24 22:10:01 +0430

    added variable indentifier

---
+++ 4. Java Variables/VariableIndentifier.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class VariableIndentifier {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // Good
+        int minutesPerHour = 60;
+        
+        System.out.println(minutesPerHour);
+
+        // OK, but not so easy to understand what m actually is
+        int m = 60;
+        
+        System.out.println(m);
+        
+    }
+}
