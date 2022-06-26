41b2b865bda7ad38082af58249a2d236ae371f0e
Yasin <yasinm289@gmail.com>
2022-06-26 10:15:38 +0430

    added assignment operators example

---
+++ 5. Java Operators/AssignmentOperator.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class AssignmentOperator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int x = 10; // Assignment operators are used to assign values to variables.
+        x += 5;     // The addition assignment operator (+=) adds a value to a variable.
+        System.out.print(x);
+        
+        // Other assignment operators: -= , *= , /= , %=
+    }
+}
