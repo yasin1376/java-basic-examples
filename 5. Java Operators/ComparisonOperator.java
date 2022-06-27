434ce7ec164d9cd0c57c712a12d6501f5d31a117
Yasin <yasinm289@gmail.com>
2022-06-27 08:20:42 +0430

    added comparison operator example

---
+++ 5. Java Operators/ComparisonOperator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class ComparisonOperator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // Comparison operators are used to compare two values and returns true or false
+        
+        // ==	Equal to
+        // !=	Not equal
+        // >	Greater than
+        // <	Less than
+        // >=	Greater than or equal to
+        // <=	Less than or equal to
+       
+        if (3 < 4){
+            System.out.print("3 is less than 4");
+        }
+        
+    }
+}
