410723e9a883eb0320ab1235bf1567508e1e576a
Yasin <yasinm289@gmail.com>
2022-06-27 08:29:36 +0430

    added logical operator

---
+++ 5. Java Operators/LogicalOperator.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class LogicalOperator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // Logical operators are used to determine the logic between variables or values
+        
+        // && 	Logical and	Returns true if both statements are true
+        // || 	Logical or	Returns true if one of the statements is true
+        // !	Logical not	Reverse the result, returns false if the result is true
+        
+        int x = 3;
+        if (x < 5 &&  x < 10){
+            System.out.print(x);
+        }
+        
+    }
+}
