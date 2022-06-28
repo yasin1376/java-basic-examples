0532af5472698de5b18249e0672ec5a4cad9f5ab
Yasin <yasinm289@gmail.com>
2022-06-28 17:51:16 +0430

    added java math

---
+++ 7. Java Math/JavaMath.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaMath {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int x = 30;
+        int y = 40;
+        
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x, y));
+
+        // The Math.sqrt(x) method returns the square root of x
+        
+        // The Math.abs(x) method returns the absolute (positive) value of x
+        
+        // Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive)
+    }
+}
