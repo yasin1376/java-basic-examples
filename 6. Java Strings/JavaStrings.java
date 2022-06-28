78813e54fcaf623edeef2d3997de80fc4298c0c5
Yasin <yasinm289@gmail.com>
2022-06-28 17:24:18 +0430

    create folder for java strings

---
+++ 6. Java Strings/JavaStrings.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaStrings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // A String variable contains a collection of characters surrounded by double quotes
+    }
+}
