f78f85f77802d7f0e1499939d3b2b429451b236d
Yasin <yasinm289@gmail.com>
2022-06-29 18:22:36 +0430

    added if example

---
+++ 8. Java If Else/JavaIf.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaIf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+          System.out.println("x is greater than y");
+        }
+    }
+}
