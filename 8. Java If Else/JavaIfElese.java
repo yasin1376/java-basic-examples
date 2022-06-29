0a51f6881d4841d4fd4a69bf5ae0fea58cc14b5a
Yasin <yasinm289@gmail.com>
2022-06-29 18:28:10 +0430

    added if else example

---
+++ 8. Java If Else/JavaIfElese.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaIfElese {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int time = 20;
+        if (time < 18) {
+          System.out.println("Good day.");
+        } else {
+          System.out.println("Good evening.");
+        }
+        
+    }
+}
