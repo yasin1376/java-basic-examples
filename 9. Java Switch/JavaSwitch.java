21806b05fb7af748f4138b67cb1761b2cf3279f3
Yasin <yasinm289@gmail.com>
2022-06-30 10:38:47 +0430

    added java switch

---
+++ 9. Java Switch/JavaSwitch.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaSwitch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // Use the switch statement to select one of many code blocks to be executed.
+        
+        int x = 10;
+        
+        switch(x){
+            case 10:
+                System.out.print("X is 10");
+                break;
+            default:
+                System.out.print("X is not 10");
+        }
+        
+    }
+}
