b90a42ca70e06d9d6669dee912fbdafc7eec7f9c
Yasin <yasinm289@gmail.com>
2022-06-30 18:03:02 +0430

    added java short if else age

---
+++ 8. Java If Else/JavaShortIfElseAge.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaShortIfElseAge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int age = 4;
+              
+        String status = age > 18 ? "You are younger" : "You are a child";
+
+        System.out.print(status);
+    }
+}
