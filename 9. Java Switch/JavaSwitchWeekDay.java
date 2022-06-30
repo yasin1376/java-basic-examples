fd1a576d9d0c748a43dd4524fd14188c5af47659
Yasin <yasinm289@gmail.com>
2022-06-30 10:42:06 +0430

    added java switch week day

---
+++ 9. Java Switch/JavaSwitchWeekDay.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaSwitchWeekDay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+    }
+}
