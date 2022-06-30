e2d0f3a7d2d7f1a7ccc8452f363af887cb74d413
Yasin <yasinm289@gmail.com>
2022-06-30 10:45:27 +0430

    added java for loop

---
+++ 10. Java For Loop/JavaForLoop.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaForLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
