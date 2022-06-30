771bb0c5ebea29d335d1efbc128df135425e7501
Yasin <yasinm289@gmail.com>
2022-06-30 10:48:13 +0430

    added java while loop:

---
+++ 11. Java While Loop/JavaWhileLoop.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaWhileLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int i = 0;
+        while (i < 5) {
+          System.out.println(i);
+          i++;
+        }
+    }
+}
