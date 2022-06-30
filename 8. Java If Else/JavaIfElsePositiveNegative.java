baa2d79bf54016742167fe9cd7297c6121288b22
Yasin <yasinm289@gmail.com>
2022-06-30 17:43:00 +0430

    added java if else positive and negative

---
+++ 8. Java If Else/JavaIfElsePositiveNegative.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaIfElsePositiveNegative {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int number = -13;
+        
+        if (number == 0){
+            
+            System.out.println("Zero");
+            
+        }else if (number > 0){
+            
+            System.out.println("Positive number");
+            
+        }else {
+            
+            System.out.println("Negative number");
+        
+        }
+        
+    }
+}
