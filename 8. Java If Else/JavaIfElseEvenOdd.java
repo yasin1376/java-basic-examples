a88d1449a4b8aa2e5f33c566aeb25764faa8dfa3
Yasin <yasinm289@gmail.com>
2022-06-30 17:38:52 +0430

    added if even odd example

---
+++ 8. Java If Else/JavaIfElseEvenOdd.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaIfElseEvenOdd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int number = 13;
+        
+        if (number % 2 == 0){
+            
+            System.out.println("Even number");
+            
+        }else {
+            
+            System.out.println("Odd number");
+            
+        }
+    }
+}
