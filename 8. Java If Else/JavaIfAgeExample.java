f2b266469bee68238f9df151a6b41b43c5738b7f
Yasin <yasinm289@gmail.com>
2022-06-30 17:34:37 +0430

    added java if age example

---
+++ 8. Java If Else/JavaIfAgeExample.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaIfAgeExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        // defining the age variable
+        int age = 20;
+        
+        // checking the age
+        
+        if (age > 18){
+            
+            System.out.println("Age is greater than 18");
+            
+        }
+        
+    }
+}
