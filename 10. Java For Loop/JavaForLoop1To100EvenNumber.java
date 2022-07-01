007e756ebaf581d2a3cddbca07f3bc64e9955396
Yasin <yasinm289@gmail.com>
2022-07-01 09:52:43 +0430

    added java for loop 1 - 100 even numbers

---
+++ 10. Java For Loop/JavaForLoop1To100EvenNumber.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaForLoop1To100EvenNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+
+        for (int x = 1; x <= 100; x++) {
+            
+            if (x % 2 == 0) {
+                
+                System.out.println(x);
+                
+            }
+        }
+    }
+}
