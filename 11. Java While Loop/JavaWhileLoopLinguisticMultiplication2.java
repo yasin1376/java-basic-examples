92177a3777f4eda4ac812625882a57161e75476c
Yasin <yasinm289@gmail.com>
2022-07-01 10:00:14 +0430

    added java multiplition 2 while loop

---
+++ 11. Java While Loop/JavaWhileLoopLinguisticMultiplication2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaWhileLoopLinguisticMultiplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        int z = 1;
+     
+        while (z <= 10) {
+
+            System.out.println(z + " * 2 = " + z * 2);
+
+            z++;
+        }
+    }
+}
