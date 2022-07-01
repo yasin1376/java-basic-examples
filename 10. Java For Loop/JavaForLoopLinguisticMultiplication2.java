5343322409ceec0704ed04247037d335735e5d83
Yasin <yasinm289@gmail.com>
2022-07-01 09:56:32 +0430

    added java for loop multiplition 2

---
+++ 10. Java For Loop/JavaForLoopLinguisticMultiplication2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaForLoopLinguisticMultiplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+
+        for (int i = 1; i <= 10; i++) {
+            
+            System.out.println(i + " * 2 = " + (i * 2));
+            
+        }
+    }
+}
