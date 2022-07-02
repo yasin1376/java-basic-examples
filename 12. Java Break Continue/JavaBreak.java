e5aa7f381b424e85b3e737f4c2e56add8c311e40
Yasin <yasinm289@gmail.com>
2022-07-02 11:57:02 +0430

    added java break example

---
+++ 12. Java Break Continue/JavaBreak.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author yasin
+ */
+public class JavaBreak {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        for (int i = 1 ; i <= 20 ; i++){
+           System.out.println(i);
+           if (i == 10){
+               break;
+           }
+       }
+        
+    }
+}
